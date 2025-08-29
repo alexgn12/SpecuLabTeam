d13e0cb7cf0ed6f1e35dd586da0f50cfdbac10a9
eve <jami.159261@e.unavarra.es>
2025-08-29 10:16:47 +0200

    Añadir propiedad ApartmentCount a Building
    Se ha añadido la propiedad `ApartmentCount` de tipo `string` en la clase `Building`, inicializada como cadena vacía. Se ha actualizado el modelo de la base de datos para incluir esta nueva columna, y se ha creado la migración `AparmentCountImplementation` para reflejar el cambio en la tabla `Buildings`. Además, se ha configurado la nueva columna como requerida en el contexto de la base de datos.

---
+++ PrototipoApiSolucion/PrototipoApi/Migrations/20250829081550_AparmentCountImplementation.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrototipoApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AparmentCountImplementation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApartmentCount",
+                table: "Buildings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApartmentCount",
+                table: "Buildings");
+        }
+    }
+}
