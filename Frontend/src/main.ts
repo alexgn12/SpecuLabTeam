54a2e57a5b33ec1e0ec8c0d60f3f90a24ed7643b
tomas <tomasorejas1@gmail.com>
2025-09-02 10:57:10 +0200

    feat: refactor patrimony page layout and enhance responsiveness with new styles

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -2,10 +2,14 @@ import { platformBrowser } from '@angular/platform-browser';
 import { AppModule } from './app/app-module';
 import { registerables } from 'chart.js';
 import { Chart } from 'chart.js';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
 
 Chart.register(...registerables);
 
 platformBrowser().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true,
+  providers: [provideAnimations()]
 })
   .catch(err => console.error(err));
+  
